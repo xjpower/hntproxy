c6c5fcafb4097bce991cd1e9af679ff66413572b
zlqzh <zlq@qq.com>
2022-05-07 11:15:44 +0800

    Update aliyun-l2tp-hnt.sh

--- aliyun-l2tp-hnt.sh
+++ aliyun-l2tp-hnt.sh
@@ -477,6 +477,7 @@ COMMIT
 :OUTPUT ACCEPT [0:0]
 :POSTROUTING ACCEPT [0:0]
 -A PREROUTING -d ${NP}/32 -p tcp -m tcp --dport 44158 -j DNAT --to-destination ${iprange}.${userip}
+-A PREROUTING -d ${NP}/32 -p udp -m udp --dport 1680 -j DNAT --to-destination ${iprange}.${userip}
 -A POSTROUTING -s ${iprange}.0/24 -j SNAT --to-source ${NP}
 COMMIT
 EOF
@@ -485,6 +486,7 @@ EOF
             iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
             iptables -I FORWARD -s ${iprange}.0/24  -j ACCEPT
             iptables -t nat -A PREROUTING -d ${NP}/32 -p tcp -m tcp --dport 44158 -j DNAT --to-destination ${iprange}.${userip}
+            iptables -t nat -A PREROUTING -d ${NP}/32 -p udp -m udp --dport 1680 -j DNAT --to-destination ${iprange}.${userip}
             iptables -t nat -A POSTROUTING -s ${iprange}.0/24 -j SNAT --to-source ${NP}
             /etc/init.d/iptables save
         fi
